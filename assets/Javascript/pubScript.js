ac92c2d3fe6dbf68abd15423d2448d1bc59a5b3f
LilTimmyBynum <timothy_ja_bynum@yahoo.com>
2017-06-05 21:11:23 -0700

    rough commit version

---
+++ assets/Javascript/pubScript.js
@@ -0,0 +1,106 @@
+// JavaScript Document
+
+var infowindowContent;
+
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 37, lng: -98 },
+        zoom: 4,
+    });
+
+    input = document.getElementById('searchItem');
+
+
+    var autocomplete = new google.maps.places.Autocomplete( input, { placeIdOnly: true } );
+		autocomplete.bindTo('bounds', map);
+   
+    var infowindow = new google.maps.InfoWindow();
+    	infowindow.setContent(infowindowContent);
+	
+    var geocoder = new google.maps.Geocoder();
+	
+    autocomplete.addListener('place_changed', function() {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+
+        if (!place.place_id) {
+            return;
+        }
+ 
+		geocoder.geocode({ 'placeId': place.place_id }, function(results, status) {
+
+			if (status !== 'OK') {
+				window.alert('Geocoder failed due to: ' + status);
+				return;
+            	}
+		
+			map.setZoom(14);
+			map.setCenter(results[0].geometry.location);
+
+
+			var pubs = new google.maps.InfoWindow();
+			var service = new google.maps.places.PlacesService(map);
+
+
+			obj='https://maps.googleapis.com/maps/api/place/textsearch/json?query=pub&location=37.7930,-122.4161&radius=500&key=AIzaSyCpzcx4xPG0GtyMrFs83Mxa0Vm0V4TCyKo';
+
+			console.log(obj);
+
+			function logResults(json){
+				console.log(json);
+			}
+
+			$.ajax({
+				method: "get",
+				contentType : "application/json",
+				url: obj,
+				dataType: "jsonp",
+				jsonpCallback: "logResults"
+			});
+			
+			
+		bars = new google.maps.InfoWindow();
+		service = new google.maps.places.PlacesService(map);
+		
+		service.nearbySearch({
+		  	location: results[0].geometry.location,
+		  	radius: 500,
+		  	name: 'pub'
+		}, callback);
+		
+	 });
+  });
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            	createMarker(results[i]);
+            	console.log(results[i]); 
+				console.log(results[i]);
+        }
+    }
+}
+	
+function createMarker(place) {
+   var id = place.id;
+	console.log(id);
+   var marker = place.location;
+   var image = 'images/beer.png'; 
+   marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+		icon: image,
+		animation: google.maps.Animation.DROP,  
+        title: place.name        
+    });	
+
+    google.maps.event.addListener(marker, 'click', function() {
+        pubs.setContent(place.name);
+        pubs.open(map, this);
+    });
+	
+	document.getElementById('#photo');
+}
+
---
+++ assets/Javascript/working.js
@@ -0,0 +1,139 @@
+// Beer scripts.js
+
+
+
+
+
+
+
+
+var myLimit = 10;
+// var myRating = "y";
+
+
+
+ // google maps API key =   AIzaSyAHB_29w-sW3VDI_i41S9D7EbeXrKbpT4Q
+
+
+
+function makeGifs(buttonText) {
+        var imgDiv;
+
+        var myQueryURL = "http://api.giphy.com/v1/gifs/search?q=" + buttonText + "&limit=" + myLimit + "&api_key=dc6zaTOxFJmzC";
+        console.log(buttonText);
+        $.ajax({
+            url: myQueryURL,
+            method: "GET"
+        }).done(function(response) {
+            imgDiv = $("<div></div>");
+            $("#myObject").append(imgDiv);            
+
+            for (count = 0; count < myLimit; count++) {
+                var imgFrame = $("<figure></figure>");
+                var myImageStillURL = response.data[count].images.original_still.url;
+                var myImageAniURL = response.data[count].images.original.url;
+                var myImageRating = response.data[count].rating;
+                var myImage = $("<img>");
+
+                console.log(response.data[count].id);
+                console.log(response.data[count].rating);
+
+                myImage.attr("src", myImageStillURL);
+                myImage.attr("still", myImageStillURL);
+                myImage.attr("moving", myImageAniURL);
+                myImage.attr("status", "stop");
+                myImage.attr("picSize", "normal");
+
+                myImage.addClass("imageStyle");
+
+                myImage.on("click", function() {
+                    var typeTest = this.src;
+                    if ($(this).attr("status") === "stop") {
+                        $(this).attr("src", $(this).attr("moving"));
+                        $(this).attr("status", "go");
+                    } else {
+                        $(this).attr("src", $(this).attr("still"));
+                        $(this).attr("status", "stop");
+                    };
+                });
+
+                imgFrame.addClass("picFrame");
+                imgFrame.append(myImage);
+                imgFrame.append("<figcaption>" + "Rating: " + myImageRating + "</figcaption>");
+
+                
+                var actionInnerFrame = $("<div></div>");
+                actionInnerFrame.addClass("aos-item__inner");
+                actionInnerFrame.append(imgFrame);
+
+                
+                var actionFrame = $("<div></div>");
+                actionFrame.addClass("aos-item");
+
+                
+                actionFrame.attr("data-aos", "flip-right");
+                actionFrame.append(actionInnerFrame);
+                imgDiv.append(actionFrame);
+            };
+        });
+        $("#myObject").append(imgDiv);
+    };
+
+function sizeBtnAction() {
+    var theSizeBtn = $("#resizeBtn");
+    theSizeBtn.on("click", function() {
+
+        if ($("img").attr("picSize") == "normal") {
+                        $("img").removeClass("imageStyle");
+                        $("figure").removeClass("picFrame");
+                        $("theObject").removeClass("picFrame");
+
+                        $("img").addClass("smImageStyle");
+                        $("figure").addClass("smPicFrame");
+                        $("theObject").addClass("smFrame");
+
+                        $("img").attr("picSize", "small");
+
+
+                    } else {
+                        $("img").removeClass("smImageStyle");
+                        $("figure").removeClass("smPicFrame");
+                        $("theObject").removeClass("smPicFrame");
+
+                        $("img").addClass("imageStyle");
+                        $("figure").addClass("picFrame");
+                        $("theObject").addClass("smPicFrame");
+
+                        $("img").attr("picSize", "normal");
+                    };
+
+    });
+
+
+}
+
+
+function goBtnAction() {
+
+	var theGoBtn = $("#goBtn");
+	theGoBtn.on("click", function() {
+			
+			var searchText = ($("#searchItem").val().trim());
+                console.log(searchText);
+
+			if(searchText != "") {
+
+				$("#message").html("TIM BYNUM");
+
+                makeGifs(searchText);
+			}
+			else {
+				$("#message").html("NULL !!!!!!!");
+				console.log(searchText);
+			}
+	});
+}
+
+
+goBtnAction();
+// sizeBtnAction();
